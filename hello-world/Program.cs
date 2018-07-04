67295756cec918ae7fa7f40e0d3609f6488dcd30
PayneQin <qinyuanpei@chinasofti.com>
2018-07-04 10:42:54 +0800

    Init Commit

---
+++ hello-world/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var msg = "Hello .NET Core";
+            using(var fileStream = new FileStream("output.txt",FileMode.Create,FileAccess.Write))
+            {
+                var bytes = Encoding.UTF8.GetBytes(msg);
+                fileStream.Write(bytes,0,bytes.Length);
+                Console.WriteLine(msg);
+            }
+
+        }
+    }
+}
